a8af4314cd70e8afbfa57b39aa236608eb7439a8
Yshnavjandj <vyshnav.ram@jobinandjismi.com>
2024-10-07 19:53:14 +0530

    OTP-7873 refactor: import object

--- OTP-7873 Identify Change in Address/src/FileCabinet/SuiteScripts/jobinAndjismi/OTP7873-Identify_change_in_Address/jj_ue_identify_change_in_address_otp7873.js
+++ OTP-7873 Identify Change in Address/src/FileCabinet/SuiteScripts/jobinAndjismi/OTP7873-Identify_change_in_Address/jj_ue_identify_change_in_address_otp7873.js
@@ -2,37 +2,16 @@
  * @NApiVersion 2.1
  * @NScriptType UserEventScript
  */
-/*******************************************************************************************************************
- * 
- * OTP-7873: Identify change in Address
- * 
- * Author: Jobin And Jismi IT Services
- * 
- * *****************************************************************************************************************
- * 
- * Date created: 30-September-2024
- * 
- * Description: This script is for identifying whether a new address is added or existing address is changed.
- * 
- * RVISION HISTORY 1.0
- * 
- *******************************************************************************************************************
- */
 define(['N/record'],
     /**
  * @param{record} record
  */
     (record) => {
-
         /**
          * Checks whether the address is changed or new address is added and check or uncheck the custom box accordingly.
          * @param {Object} scriptContext 
-         * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
-         * @param {Record} scriptContext.newRecord - New record
-         * @param {Record} scriptContext.oldRecord - Old record
          * @return {Boolean} returns true or false
          */
-
         const checkBox = (scriptContext) => {
             try {
                 if(scriptContext.type === 'edit') {
@@ -53,7 +32,7 @@ define(['N/record'],
                             value: true
                         });
                     }else {
-                        for(let i = 0; i < oldLineCount; i++) {
+                        for(let i = 0; i < oldLineCount; i++) { // use oldLineCount or newLineCount
                             let oldAddress = oldRecord.getSublistText({
                                 sublistId: 'addressbook',
                                 fieldId: 'addressbookaddress_text',
@@ -74,6 +53,15 @@ define(['N/record'],
                                 break;
                             }
                         }
+
+                        // let sublist = newRecord.getSublist({
+                        //     sublistId: 'addressbook'
+                        // });
+
+                        // let column = sublist.getColumn({
+                        //     fieldId: 'addressbookaddress'
+                        // });
+                        // addrNew.push(column);
                     }
 
                     log.debug("old address: ",addrOld);
@@ -84,6 +72,19 @@ define(['N/record'],
                 log.debug(error.message);
                 log.debug(error.cause);
             }
+        }
+
+        /**
+         * Defines the function definition that is executed before record is loaded.
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.newRecord - New record
+         * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
+         * @param {Form} scriptContext.form - Current form
+         * @param {ServletRequest} scriptContext.request - HTTP request information sent from the browser for a client action only.
+         * @since 2015.2
+         */
+        const beforeLoad = (scriptContext) => {
+
         }
 
         /**
@@ -98,8 +99,18 @@ define(['N/record'],
             checkBox(scriptContext);
         }
 
-        return {
-            beforeSubmit
+        /**
+         * Defines the function definition that is executed after record is submitted.
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.newRecord - New record
+         * @param {Record} scriptContext.oldRecord - Old record
+         * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
+         * @since 2015.2
+         */
+        const afterSubmit = (scriptContext) => {
+
         }
+
+        return {beforeLoad, beforeSubmit, afterSubmit}
 
     });
